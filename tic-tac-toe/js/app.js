e382ba0ed24129851777c9b535d0c99a75fee0ec
joecih <joecih@gmail.com>
2016-03-08 13:09:59 -0500

    Updates to tic-tac-toe
    Styling, added menu1.jpg and updates to app.js

--- tic-tac-toe/js/app.js
+++ tic-tac-toe/js/app.js
@@ -1,15 +1,56 @@
-// Initally
+var winners = [
+  ['b1','b2','b3'],
+  ['b4','b5','b6'],
+  ['b7','b8','b9'],
+  ['b1','b5','b9'],
+  ['b2','b5','8'],
+  ['b3','b5','b7'] 
+];
 
-// document.getElementById("app-icons").style.display = "none";
+var whosTurn = 1;
+updatePlayer();
 
-function showGoogleApps() {
+function addSelection(who){
+    
+    if (whosTurn == 1) {
+        // it's X's turn, put an X in empty square.
+        who.innerHTML = 'X';
+        whosTurn = 2;
+    } else {
+        // it has tobe O's turn .. put an O in.
+        who.innerHTML = 'O';
+        whosTurn = 1;
+    }
+    
+}
 
-    var e = document.getElementById("app-icons");
+function showMenu() {
+
+    var e = document.getElementById("drop-down-menu");
     
     if (e.style.display == 'block' || e.style.display == '') {
-        document.getElementById("app-icons").style.display = "none";
+        e.style.display = "none";
     } else {
-        document.getElementById("app-icons").style.display = "block";
+        e.style.display = "block";
     }
 
+}
+
+function update2ndPlayerField() {
+    var eTick = document.getElementById("player-tick");
+    var ePlayerName = document.getElementById("player-name");
+    
+    if (eTick.checked == true) {
+        ePlayerName.disabled = false;
+        ePlayerName.style.backgroundColor = "lightgoldenrodyellow";
+    } else {
+        ePlayerName.disabled = true;
+        ePlayerName.style.backgroundColor = "darkgrey";
+    }
+}
+
+function updatePlayerNames() {
+    var eInputName = document.getElementById("player-name");
+    var ePlayer1Name = document.getElementById("player1-name");
+    var ePlayer2Name = document.getElementById("player2-name");
 }
